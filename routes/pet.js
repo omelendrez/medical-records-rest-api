49582eed27f6f70698062dbf6b8ebbad2897de4e
omelendrez <omar.melendrez@gmail.com>
2020-05-09 02:39:38 -0300

    Refactor routes

---
+++ routes/pet.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const Pet = require('../controllers/pet')
+
+router.get('/', Pet.getAll)
+router.get('/inactive', Pet.getInactive)
+router.get('/:id', Pet.getById)
+router.post('/', Pet.create)
+router.delete('/:id', Pet.deleteRecord)
+router.put('/:id', Pet.restoreRecord)
+
+module.exports = router
