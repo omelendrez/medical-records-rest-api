070de23782d9ad0724ec15c7aba7437775945f31
omelendrez <omar.melendrez@gmail.com>
2020-05-21 21:34:02 -0300

    Add vaccination model

---
+++ models/vaccination.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Vaccination = sequelize.define('vaccination', {
+    customerId: DataTypes.INTEGER,
+    petId: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+    vaccination: DataTypes.STRING(500),
+    nextAppointment: {
+      type: DataTypes.DATE,
+      defaultValue: null
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      defaultValue: 0
+    },
+    paymentMethod: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    },
+    paid: {
+      type: DataTypes.DECIMAL(10, 2),
+      defaultValue: 0
+    },
+    statusId: {
+      type: DataTypes.TINYINT,
+      defaultValue: 1
+    }
+  }, {});
+  Vaccination.associate = function (models) {
+    // associations can be defined here
+  };
+  Vaccination.prototype.data = function () {
+    let json = this.toJSON()
+    return json
+  }
+  return Vaccination;
+};
--- routes/vaccination.js
+++ routes/vaccination.js
@@ -4,5 +4,12 @@ const router = express.Router()
 const Vaccination = require('../controllers/vaccination')
 
 router.get('/', Vaccination.getAll)
+router.get('/inactive', Vaccination.getInactive)
+router.get('/programmed-visits', Vaccination.getnextAppointments)
+router.get('/by-pet/:id', Vaccination.getByPet)
+router.get('/:id', Vaccination.getById)
+router.post('/', Vaccination.create)
+router.delete('/:id', Vaccination.deleteRecord)
+router.put('/:id', Vaccination.restoreRecord)
 
 module.exports = router
