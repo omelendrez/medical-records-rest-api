9b044a9590ef783ab31854c6fa85f5b6cb0d7c22
Omar Melendrez <omar.melendrez@gmail.com>
2022-07-18 22:54:37 -0300

    Update heroku downloader

--- backup/download_from_heroku.bat
+++ backup/download_from_heroku.bat
@@ -1,7 +1,3 @@
 @echo off
-set backuptime=heroku_b3627cd1b200890_%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
-echo %backuptime%
-
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u %VMR_USER% --routines --column-statistics=0 --no-create-db -h us-cdbr-east-06.cleardb.net --password=%VMR_PASSWORD% heroku_b3627cd1b200890 > %backuptime%.sql
-
-pause
+set file_name=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_heroku_188f5830c19670d_PRODUCTION.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u %VETEAPP_USER% --routines --column-statistics=0 --no-create-db --no-tablespaces -h us-cdbr-east-06.cleardb.net --password=%VETEAPP_PASSWORD% heroku_188f5830c19670d > %file_name%.sql
--- backup/download_from_local.bat
+++ backup/download_from_local.bat
@@ -1,4 +1,3 @@
 @echo off
 set file_name=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_heroku_188f5830c19670d.sql
 "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -h 127.0.0.1  -u %ROOT_USER% --routines --column-statistics=0 --no-create-db --no-set-names --password=%ROOT_PASSWORD% --databases heroku_188f5830c19670d > %file_name%
-pause
